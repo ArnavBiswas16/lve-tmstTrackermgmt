705b84087d0cc8fea43d861815fe8b68cd4b75cd
Arnav Biswas <abiswas@altimetrik.com>
2026-02-05 09:32:22 +0530

    Initial commit

---
+++ src/main/java/com/alt/lve_tmst_mgmt/LveTmstMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.alt.lve_tmst_mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LveTmstMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LveTmstMgmtApplication.class, args);
+	}
+
+}
